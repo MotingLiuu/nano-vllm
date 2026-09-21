0ab3894332b852e6d9da151b59f2ccf72cb28368
MotingLiuu <moting.liuu@gmail.com>
2026-09-21 17:49:11 +0800

    added annotations

--- nanovllm/engine/scheduler.py
+++ nanovllm/engine/scheduler.py
@@ -9,12 +9,15 @@ class Scheduler:
 
     def __init__(self, config: Config):
         self.max_num_seqs = config.max_num_seqs
+        # the max num of sequences in this batch
         self.max_num_batched_tokens = config.max_num_batched_tokens
         self.eos = config.eos
         self.block_size = config.kvcache_block_size
         self.block_manager = BlockManager(config.num_kvcache_blocks, config.kvcache_block_size)
         self.waiting: deque[Sequence] = deque()
+        # seqs waiting for prefill
         self.running: deque[Sequence] = deque()
+        # seqs decoding
 
     def is_finished(self):
         return not self.waiting and not self.running
@@ -22,7 +25,10 @@ class Scheduler:
     def add(self, seq: Sequence):
         self.waiting.append(seq)
 
+    # If there is seq need to be prefilled, return True
+    # do prefill or decode, just one of them
     def schedule(self) -> tuple[list[Sequence], bool]:
+        # return tuple[list[]]
         scheduled_seqs = []
         num_batched_tokens = 0
 
@@ -31,6 +37,7 @@ class Scheduler:
             seq = self.waiting[0]
             remaining = self.max_num_batched_tokens - num_batched_tokens
             if remaining == 0:
+            # check whether there is token budget
                 break
             if not seq.block_table:
                 num_cached_blocks = self.block_manager.can_allocate(seq)
@@ -44,8 +51,11 @@ class Scheduler:
             if not seq.block_table:
                 self.block_manager.allocate(seq, num_cached_blocks)
             seq.num_scheduled_tokens = min(num_tokens, remaining)
+
             num_batched_tokens += seq.num_scheduled_tokens
             if seq.num_cached_tokens + seq.num_scheduled_tokens == seq.num_tokens:
+                # done seq prefill, WAITTING->RUNNING
+                # pop from waiting, append to running
                 seq.status = SequenceStatus.RUNNING
                 self.waiting.popleft()
                 self.running.append(seq)
@@ -58,25 +68,34 @@ class Scheduler:
         while self.running and len(scheduled_seqs) < self.max_num_seqs:
             seq = self.running.popleft()
             while not self.block_manager.can_append(seq):
+                # LIFO, preempt from the end of self.running
                 if self.running:
                     self.preempt(self.running.pop())
                 else:
+                    # Question: why preempt seq self?
+                    # if break here, the assert will fail
+                    # Answer: This means memo used by seq can not be santisfied even using all of the memo.
                     self.preempt(seq)
                     break
             else:
+                # If while loop end for true, execute else
+                # add seq to scheduled
                 seq.num_scheduled_tokens = 1
                 seq.is_prefill = False
                 self.block_manager.may_append(seq)
                 scheduled_seqs.append(seq)
         assert scheduled_seqs
         self.running.extendleft(reversed(scheduled_seqs))
+        # reverse maintain the order of original self.running
         return scheduled_seqs, False
 
     def preempt(self, seq: Sequence):
+        # free the kv cache of seq, and move it to waiting
         seq.status = SequenceStatus.WAITING
         seq.is_prefill = True
         self.block_manager.deallocate(seq)
         self.waiting.appendleft(seq)
+        # add seq to left of waiting, to ensure it would be the first one to be prefilled when memo avilable
 
     def postprocess(self, seqs: list[Sequence], token_ids: list[int], is_prefill: bool):
         for seq, token_id in zip(seqs, token_ids):
@@ -84,9 +103,22 @@ class Scheduler:
             seq.num_cached_tokens += seq.num_scheduled_tokens
             seq.num_scheduled_tokens = 0
             if is_prefill and seq.num_cached_tokens < seq.num_tokens:
+                # Question: this would only happen when chunked prefill?
+                # what is the difference between break and continue?
+                # Answer: no difference.
                 continue
             seq.append_token(token_id)
             if (not seq.ignore_eos and token_id == self.eos) or seq.num_completion_tokens == seq.max_tokens:
+                # set the status to FINISHED
                 seq.status = SequenceStatus.FINISHED
                 self.block_manager.deallocate(seq)
                 self.running.remove(seq)
+
+
+
+
+
+
+
+
+
